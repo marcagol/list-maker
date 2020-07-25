f7533af5fbd93b2008d26fa81fc583dee7944fb5
Marcelina Golinska <marcagol@gmail.com>
2020-07-25 21:38:03 +0200

    added new task addition function

---
+++ js/script.js
@@ -0,0 +1,52 @@
+{
+    const tasks = [
+        {
+            content: "mleko",
+            done: false,
+        },
+        {
+            content: "płatki",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li
+            ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+            > 
+            ${task.content}
+            </li>
+            `;
+        }
+        document.querySelector("js-tasks").innerHTML = htmlString;
+        //Błąd w linii 25: Cannot set property 'innerHTML' of null 
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector("js-form");
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector("js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+
+            tasks.push({
+                content: newTaskContent,
+            });
+
+            render();
+        });
+    };
+
+    init();
+}
